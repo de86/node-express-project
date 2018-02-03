3ed6ed064575b3b61a1be9bf9d63966176f67efa
de86 <daelliott86@gmail.com>
2018-02-03 22:03:21 +0000

    Extracted routes into their own folder Added Morgan logging. Amended eslint config.

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const express      = require ( 'express' );
 const cookieParser = require ( 'cookie-parser' );
 const bodyParser   = require ( 'body-parser' );
+const morgan       = require ( 'morgan' );
 const config       = require ( './config' );
+const routes       = require ( './routes' );
 
 const app = express();
 
 // Middleware
 app.use ( bodyParser.json());
 app.use ( cookieParser());
-
+app.use ( morgan( 'dev' ));
 
 // Routes
-app.get ( '/', ( req, res ) => 
-  res.send ( 'Hello World' ));
+routes( app );
 
 
 // Start App
 app.listen( config.PORT, () =>
   console.log( `App listening on port ${config.PORT}, Happy hunting` ));
 
+
+// ToDo: 
+// - Move routes into its own module
+// - Configure Morgan or other logger
